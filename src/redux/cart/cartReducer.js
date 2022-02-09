81aab7fd209adcf798c72d60114604ac6623b55a
guruprakash-aus <rajendranguruprakash@gmail.com>
2022-02-09 14:05:23 +1100

    addItem and RemoveItem in CheckoutPage

--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -1,9 +1,9 @@
 import CartActionTypes from "./cartTypes";
-import { addItemToCart } from "./cartUtils";
+import { addItemToCart, removeItemFromCart } from "./cartUtils";
 
 const initialState = {
-    hidden: true,
-    cartItems: []
+  hidden: true,
+  cartItems: [],
 };
 
 const cartReducer = (state = initialState, { type, payload }) => {
@@ -13,8 +13,22 @@ const cartReducer = (state = initialState, { type, payload }) => {
 
     case CartActionTypes.ADD_ITEM:
       return {
-        ...state, cartItems: addItemToCart(state.cartItems, payload)
+        ...state,
+        cartItems: addItemToCart(state.cartItems, payload),
+      };
+    
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, payload),
       }
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.id !== payload.id
+        ),
+      };
     default:
       return state;
   }
