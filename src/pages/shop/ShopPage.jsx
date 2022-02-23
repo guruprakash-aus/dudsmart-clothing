cb0b0de5ac8dc3e961d238c030c2ee7bf231d9b2
guruprakash-aus <rajendranguruprakash@gmail.com>
2022-02-23 14:22:15 +1100

    Add a WithSpinner and modify how to get collections from firestore db

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -3,22 +3,40 @@ import CollectionsOverview from "../../components/CollectionsOverview/Collection
 import { useLocation } from "react-router-dom";
 import { connect } from "react-redux";
 
-
-import { convertCollectionsSnapshotToMap, firestore } from "../../firebase/utils";
+import {
+  convertCollectionsSnapshotToMap,
+  firestore,
+} from "../../firebase/utils";
 import { updateCollections } from "../../redux/shop/shopActions";
 
-const ShopPage = () => {
+const ShopPage = ({ updateCollections }) => {
   const location = useLocation();
   console.log(location);
 
   useEffect(() => {
     const collectionRef = firestore.collection("collections");
 
-    collectionRef.onSnapshot((snapshot) => {
+    // collectionRef.onSnapshot((snapshot) => {
+    //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //   updateCollections(collectionsMap);
+    //   console.log(collectionsMap)
+    // });
+
+    // Below is the direct fetch from Firestore DB which gives us a nested collection of documents. 
+    // fetch(
+    //   "https://firestore.googleapis.com/v1/projects/duds-db-22734/databases/(default)/documents/collections/"
+    // )
+    //   .then((response) => response.json())
+    //   .then((data) => console.log(data));
+
+    // The below is the promise version of the code above
+    collectionRef.get().then((snapshot) => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
       console.log(collectionsMap)
     });
-  }, []);
+
+  }, [updateCollections]);
 
   return (
     <div className='shop-page'>
@@ -28,7 +46,8 @@ const ShopPage = () => {
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+  updateCollections: (collectionsMap) =>
+    dispatch(updateCollections(collectionsMap)),
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
